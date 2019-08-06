a533592d2f6f4d6114f4b74a03482405686f9cdc
Mario Marroquin <mariomarroquin@marios-mbp.attlocal.net>
2019-08-05 23:50:06 -0700

    Add validation to Analyses model

--- app/models/analysis.rb
+++ app/models/analysis.rb
@@ -1,6 +1,10 @@
 class Analysis < ApplicationRecord
   has_one_attached :document
 
+  validates :document, presence: true
+  validates :stop_word, presence: true
+  validates :words, presence: true
+
   def common_words(n=25)
     JSON.parse(words).sort_by{ |k, v| -v}.first(n)
   end
